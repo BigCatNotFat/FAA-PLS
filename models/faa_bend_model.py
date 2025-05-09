7af0ea9ea9e39d76c522f9efde0940563d7100ad
BigCatNotFat <plmokn10527@gmail.com>
2025-05-09 17:23:04 +0800

    Changes to be committed: 	modified:   README.md 	new file:   logs/__init__.py 	new file:   models/faa_bend_model.py 	new file:   models/faa_fold_model.py 	new file:   optimizers/__init__.py 	new file:   results/__init__.py 	new file:   utils/__init__.py

---
+++ models/faa_bend_model.py
@@ -0,0 +1,14 @@
+from .faa_model_base import Faa_model_base
+
+class faa_bend_model(Faa_model_base):
+    def __init__(self, config):
+        self.config = config
+
+    def transform_antenna_positions(self, antenna_positions_original, psi):
+        pass
+
+    def directional_channel_gain(self, antenna_positions_rotated):
+        pass
+
+    def omni_channel_gain(self, antenna_positions_rotated):
+        pass
